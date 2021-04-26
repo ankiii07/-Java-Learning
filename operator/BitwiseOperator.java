fadc5088a5e4f4c51f88d490252b629a3ba126ad
ankiii07 <66520710+ankiii07@users.noreply.github.com>
2021-04-26 20:07:40 +0530

    Add files via upload

---
+++ operator/BitwiseOperator.java
@@ -0,0 +1,38 @@
+package operator;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) {
+		
+//		AND OPERATION
+//		5 ->  101
+//		6 ->  110
+//   	4 ->  100
+//		int a = 5;
+//		int b = 6;
+//		int and = a & b;
+//		
+//		System.out.println("and operation = " +and);
+		
+//		
+//   	OR OPERATION
+//		10 = 1010
+//	    13 = 1101
+//	    15 = 1111
+//	     
+//	    int a = 10;
+//		int b = 13;
+//		int or = a | b;
+//		
+//		System.out.println("or operation = " +or);
+	
+//		AIRTHMATIC RIGHT SHIFT
+//		divided by 2 
+//		int a = 15;
+//		int shift = a >> 1;
+//		
+//		System.out.println("airthmatic right shift operation = " +shift);
+
+	}
+
+}
---
+++ operator/ComparisonOperator.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class ComparisonOperator {
+
+	public static void main(String[] args) {
+		
+		// comparison operator always return boolean value : true or false
+		
+		int a = 34;
+		int b = 78;
+		
+		System.out.println(a>b);
+		 System.out.println(a<b);
+		 System.out.println(a>=b);
+		 System.out.println(a<=b);
+		 System.out.println(a==b);
+		 System.out.println(a!=b);
+	}
+
+}
